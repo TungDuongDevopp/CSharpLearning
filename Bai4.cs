005c98b1dacd7f62b061f76397384972b229e780
TungDuong-Dev <devopptungduong@gmail.com>
2026-03-29 15:33:46 +0700

    Hoan thanh bai tap buoi4

---
+++ Bai4.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Channels;
+
+namespace CSharpLearning;
+
+public class MyStack<T>
+{
+    public List<T> ListData = new List<T>();
+
+    public void Push(T item) => ListData.Add(item);
+
+    public T Peek() => ListData.Count> 0? ListData[^1] : throw new InvalidOperationException("Stack is empty.");
+
+    public T Pop()
+
+    {
+       T item = Peek();
+        ListData.RemoveAt(ListData.Count-1);
+        return item;
+    }
+
+    public bool IsEmpty() => ListData.Count == 0;
+
+    public void Clear() => ListData.Clear();
+
+    public void PrintStack()
+    {
+        for(int i = ListData.Count - 1; i >= 0; i--)
+        {
+            Console.WriteLine(ListData[i]);
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,28 @@
 ﻿
 
+using Data.MyColections;
 
 namespace CSharpLearning;
 
     internal class Program
     {
-        static void Main(string[] args)
+        
+    static void Main(string[] args)
         {
+        var stack = new MyStack<SinhVien>();
 
-        var data = new Student_Manager();
-        var kq = data.DataFromFile();
-        foreach (var item in kq)
-        {
-            Console.WriteLine(item.ToString());
-        }
-     
+        stack.Push(new SinhVien("SV001", "Nguyen Van A", 20, new DateTime(2004,08,07), 3.5, 3.8));
+        stack.Push(new SinhVien("SV002", "Nguyen Van B", 21, new DateTime(2003,11,07), 3.1, 3.2));
+        stack.Push(new SinhVien("SV003", "Trần Thị C", 22, new DateTime(1992 , 11 , 22), 2.7, 3.5));
+        Console.WriteLine("Stack ban đầu là: ");
+        stack.PrintStack();
+        var item= stack.Pop();
+        Console.WriteLine($"Phần tử bị lấy ra là:{item} ");
+        Console.WriteLine("Stack lúc sau là là: ");
+        stack.PrintStack();
 
-    }
+
+    }   
        
     }
 
