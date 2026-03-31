3e6e3a81c9870bf5f0347c9ed789469a8f1ab472
TungDuong-Dev <devopptungduong@gmail.com>
2026-03-31 12:41:34 +0700

    Hoan thien bai tap buoi 6

---
+++ Data/Interface/IEmployee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interface
+{
+    public interface IEmployee
+    {
+        public decimal TinhLuong();
+        public void DisplayInfo();
+    }
+}
---
+++ Data/Interface/IShape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interface
+{
+    public  interface IShape
+    {
+        public double TinhChuVi();
+        public double TinhDienTich();
+
+    }
+}
